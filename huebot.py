03d74691ed7862aeee64a54f1e444812d59b10a1
Leezy <leezy@ubuntu22.myguest.virtualbox.org>
2024-09-08 18:45:53 +0300

    work version huebot

--- huebot.py
+++ huebot.py
@@ -1,3 +1,4 @@
+import random
 import telebot
 from telebot import types
 
@@ -8,10 +9,18 @@ def welcome_message(message):
     bot.reply_to(message, "Напиши любой текст, и я добавлю хуе к рандомному слову")
 
 @bot.message_handler(func=lambda m: True)
-def echo_all(message):
-    new_message = list(str(message.text.split(" ")))
-    for i in range(new_message):
-        bot.reply_to(message, "хуе" + new_message[2].text)
+def echo_message(message):
+    text = message.text
+    words = text.split()
+    
+    index = random.randint(0, len(words) - 1)
+    word = words[index]
+    
+    new_word = f'хуе{word}'
+    
+    modified_text = ' '.join([w if i != index else new_word for i, w in enumerate(words)])
+    
+    bot.send_message(message.chat.id, modified_text)
 
 bot.infinity_polling()
 
