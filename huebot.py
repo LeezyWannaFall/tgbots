8104c9aad9b2bbd67b87a73f877a6492f9e6fffd
LeezyWannaFall <191919qaz@gmail.com>
2024-09-21 15:36:32 +0300

    chat_id add

--- huebot.py
+++ huebot.py
@@ -11,23 +11,28 @@ def welcome_message(message):
     keyboard.add(button1, button2)
     bot.send_message(message.chat.id, "Привет, вот что я умею: захуевить слово\nкнопка 1")
 
-@bot.message_handler(commands=['hue'])
+# @bot. message_handler(commands=['hue'])
+# def welcome_message(message):
+#     if message.text == "захуевить слово":
+#     bot.send_message(message.chat.id, "Напиши любой текст, и я добавлю хуе в рандомное место")
+
+@bot.message_handler(func=lambda message: message.text == "захуевить слово")
 def echo_message(message):
-    bot.send_message(message.chat.id, "Напиши любой текст, и я добавлю хуе в рандомное место")
-    text = message.text
-    words = text.split()
-    if words.isdigit() == True:
-        bot.send_message(message.chat.id, "напиши текст без цифр")
-    else:
+        chat_id = message.chat.id
+        text = message.text
+        words = text.split()
+        # if words.isdigit() == True:
+        #     bot.send_message(message.chat.id, "напиши текст без цифр")
         index = random.randint(0, len(words) - 1)
         word = words[index]
-        
+
         new_word = f'хуе{word}'
-        
+            
         modified_text = ' '.join([w if i != index else new_word for i, w in enumerate(words)])
-        
-        bot.send_message(message.chat.id, modified_text)
+            
+        bot.send_message(chat_id, modified_text)
 
 
-
-bot.infinity_polling()
+if __name__ == '__main__':
+    print("bot start")
+    bot.infinity_polling()
